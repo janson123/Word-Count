58a701a1c1175b1e0cc24e8d14b34866f306034b
Jared Anson <jaredanson@Jareds-MacBook-Pro.local>
2015-09-17 15:03:54 -0400

    comments

--- mywc.c
+++ mywc.c
@@ -17,8 +17,6 @@ int main(){
   int lineCount = 0;
   int firstChar = 0;
 
-  // while it is not the end of the file, it gets a new char to be processed.
-
   while((c = getchar()) != EOF){
 
     // this tests if its the first char in a file. If so the word count and char count is increased.
@@ -29,13 +27,9 @@ int main(){
       firstChar = 1;
       characterCount++;
     }else{
-      // if it encounters whitespace it increases the char count
-      // also sets a boolean to true
       if(c == ' ' | c == '\t' | c == '\r' | c == '\f' | c == '\v'){
 	characterCount++;
 	boolean = 1;
-	//when it comes across a new line, it increases line count, char count.
-	// also sets boolean to true.
       }else if(c == '\n'){
 	lineCount++;
 	characterCount++;
@@ -44,11 +38,9 @@ int main(){
 	// if neither of the conditions above are satisfied the char count is increased since
 	// that means it was just a regular character.
 	characterCount++;
-	// this checks if the boolean has been set to true. To get here means it had to encountered
-	// some type of whitespace, and now no longer are encountering it. Which means the current
+	// this checks if the boolean has been set to true. To get here means it had to encounter
+	// some type of whitespace, and now is no longer encountering it. Which means the current
 	// value is char. Going from whitespace to char indicates a word.
-	// word count is increased, and the boolean is set to false again. This makes sure that
-	// the next characters encountered are treated as such and not new words.
 	if(boolean == 1){
 	  wordCount++;
 	  boolean = 0;
@@ -56,7 +48,6 @@ int main(){
       }
     }
   }
-  // print statement
   printf("%d line(s)  %d word(s)  %d character(s)\n", lineCount, wordCount, characterCount);
 
   return(0);
